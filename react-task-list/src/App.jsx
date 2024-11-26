0aa345c227fdc6b0b6fa80fe458f70fd6765dd31
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-26 15:36:25 +0100

    ADD import tasks.js

--- react-task-list/src/App.jsx
+++ react-task-list/src/App.jsx
@@ -2,7 +2,19 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import "../data/tasks";
 
-function App() {}
+function App() {
+  <main className="main">
+    <div>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+    <div>
+      <ul></ul>
+    </div>
+  </main>;
+}
 
 export default App;
